226ef15754b7cee1253481262c2c2120a455a10a
Akmula <akmula@mail.ru>
2014-12-18 00:18:40 +0300

    Пятнашки

---
+++ src/Game.java
@@ -0,0 +1,253 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Game implements ActionListener {
+    JFrame GameFrame;
+    ImagePanel ipGame;
+    JButton jbNewGame;
+    JButton jbExit;
+    JButton jbClick;
+    JButton pressButton;
+    JPanel GameField;
+    JButton[][] jbArray;
+    Font bFont;
+    //  String recordData;
+    //  int recordClick;
+    int buttonClick;
+    int n;
+
+    Game(int n) {
+        // ---------- Окно игры
+        GameFrame = new JFrame("Пятнашки");
+        GameFrame.setIconImage(Toolkit.getDefaultToolkit().getImage("icon.png"));
+        GameFrame.setBounds(550, 150, 300, 450);
+        GameFrame.setResizable(false);
+        GameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        ipGame = new ImagePanel();
+        try {
+            ipGame.setImage(ImageIO.read(new File("bg.jpg")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ipGame.add(GameField = new JPanel());
+        ipGame.setLayout(null);
+        ipGame.add(jbNewGame = new JButton(new ImageIcon("newGame.jpg")));
+        ipGame.add(jbExit = new JButton(new ImageIcon("newExit.jpg")));
+        ipGame.add(jbClick = new JButton("Пятнашки"));
+        GameField.setBounds(25, 25, 250, 250);
+        jbClick.setBounds(25, 280, 250, 35);
+        jbNewGame.setBounds(25, 325, 120, 33);
+        jbExit.setBounds(155, 325, 120, 33);
+        jbClick.setEnabled(false);
+        GameFrame.add(ipGame);
+        GameFrame.setVisible(true);
+        bFont = new Font("Verdana", Font.BOLD, 16 - n);
+
+        // ---------- Создаем игровое поле
+        jbArray = new JButton[n][n];
+        this.n = n;
+        for (int row = 0; row < n; row++)
+            for (int col = 0; col < n; col++) {
+                jbArray[row][col] = new JButton();
+                jbArray[row][col].setFont(bFont);
+                jbArray[row][col].setFocusable(false);
+                jbArray[row][col].addActionListener(this);
+                GameField.add(jbArray[row][col]);
+            }
+        // ---------- Вешаем обработчики
+        jbNewGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createField();
+            }
+        });
+        jbExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Menu();
+                GameFrame.dispose();
+            }
+        });
+    }
+
+    // ---------- Создание поля
+    public void createField() {
+        int[] field = new int[n * n];
+        LinkedList<String> ll = new LinkedList<>();
+        GameField.setLayout(new GridLayout(n, n));
+        buttonClick = 0;
+        jbClick.setText("Вы не сделали хода");
+        for (int i = 1; i < field.length; i++) {
+            ll.add(String.valueOf(i));
+        }
+        ll.add("");
+        Collections.shuffle(ll);
+        for (int row = 0; row < n; row++)
+            for (int col = 0; col < n; col++) {
+                jbArray[row][col].setText(String.valueOf(ll.poll()));
+            }
+    }
+
+    // ---------- Условие победы ---------- //
+    public boolean victory() {
+        int count = 1;
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                String victoryText = String.valueOf(count);
+                if (!jbArray[row][col].getText().equals(victoryText) && count < n * n)
+                    return false;
+                count++;
+            }
+        }
+        return true;
+    }
+
+    public void setText() {
+        String txt;
+        if (buttonClick == 0) {
+            txt = "ход";
+        } else if (buttonClick == 1 || buttonClick == 2 || buttonClick == 3) {
+            txt = "хода";
+        } else {
+            txt = "ходов";
+        }
+        jbClick.setText("Вы сделали " + String.valueOf(buttonClick + 1) + " " + txt + "!");
+    }
+
+    public void congratulations(String recordData, int recordClick) {
+        String recordText;
+        if (buttonClick < recordClick) {
+            recordText = "Вы установили рекорд!";
+        } else recordText = "Рекорд " + recordClick + " ходов!<br>" +
+                recordData;
+        String txt = "<html><center><H2>Поздравляем!</H2><br>" +
+                "Вы выиграли!<br>" +
+                "Вы сделали: " + buttonClick + " ходов!<br>"
+                + recordText + ". <br>" +
+                "Хотите еще сыграть?</center></html>";
+        final JDialog EndGame = new JDialog(GameFrame, true);
+        JButton jbYes = new JButton("Да");
+        JButton jbNo = new JButton("Нет");
+        JLabel jlEndGame = new JLabel(txt);
+        jlEndGame.setHorizontalAlignment(SwingConstants.CENTER);
+        EndGame.setIconImage(Toolkit.getDefaultToolkit().getImage("icon.png"));
+        EndGame.setBounds(550, 250, 300, 250);
+        EndGame.setLayout(null);
+        EndGame.add(jlEndGame);
+        EndGame.add(jbYes);
+        EndGame.add(jbNo);
+        jlEndGame.setBounds(10, 10, 270, 160);
+        jbYes.setBounds(10, 180, 120, 30);
+        jbNo.setBounds(160, 180, 120, 30);
+        jbYes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createField();
+                EndGame.dispose();
+            }
+        });
+        jbNo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Menu();
+                EndGame.dispose();
+                GameFrame.dispose();
+            }
+        });
+
+        EndGame.setVisible(true);
+    }
+
+    // ---------- Ход игры --------- //
+    private boolean isEmptyButton(int row1, int col1) {
+        if (row1 < 0 || row1 >= n)
+            return false;
+        if (col1 < 0 || col1 >= n)
+            return false;
+        return jbArray[row1][col1].getText().equals("");
+    }
+
+    private void setTextB(int row1, int col1) {
+        jbArray[row1][col1].setText(pressButton.getText());
+        pressButton.setText("");
+        buttonClick++;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        pressButton = (JButton) e.getSource();
+        for (int row = 0; row < jbArray.length; row++)
+            for (int col = 0; col < jbArray[row].length; col++) {
+                if (pressButton == jbArray[row][col]) {
+                    setText();
+                    if (isEmptyButton(row - 1, col)) {
+                        setTextB(row - 1, col);
+                    } else if (isEmptyButton(row + 1, col)) {
+                        setTextB(row + 1, col);
+                    } else if (isEmptyButton(row, col - 1)) {
+                        setTextB(row, col - 1);
+                    } else if (isEmptyButton(row, col + 1)) {
+                        setTextB(row, col + 1);
+                    }
+                    if (victory()) {
+                        scoresWriteReader(0);
+                    }
+                    return;
+                }
+            }
+    }
+
+    public void scoresWriteReader(int recordClick) {
+        Date today = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        String formattedDate = sdf.format(today);
+        ArrayList<Scores> scores = new ArrayList<>();
+        String aLine;
+        try (
+                FileInputStream fin = new FileInputStream("Scores.dat");
+                BufferedReader br = new BufferedReader(new InputStreamReader(fin))) {
+            while ((aLine = br.readLine()) != null) {
+                StringTokenizer st = new StringTokenizer(aLine, " | ");
+                int level = Integer.parseInt(st.nextToken());
+                int score = Integer.parseInt(st.nextToken());
+                String dataScore = st.nextToken();
+                scores.add(new Scores(level, score, dataScore));
+            }
+            for (Scores sc : scores) {
+                if (sc.level == n) {
+                    recordClick = sc.score;
+                    String recordData = sc.dataScore;
+                    congratulations(recordData, recordClick);
+                }
+            }
+            if (buttonClick < recordClick) {
+                scores.set(n - 3, new Scores(n, buttonClick, formattedDate));
+                try (FileWriter myFile = new FileWriter("Scores.dat");
+                     PrintWriter printMyFile = new PrintWriter(myFile)) {
+                    for (Scores sc : scores) {
+                        printMyFile.println(sc.level + " | " + sc.score + " | " + sc.dataScore);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class Scores {
+        int level;
+        int score;
+        String dataScore;
+
+        Scores(int l, int s, String d) {
+            level = l;
+            score = s;
+            dataScore = d;
+        }
+    }
+}
---
+++ src/Menu.java
@@ -0,0 +1,193 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+public class Menu {
+    JFrame MenuFrame;
+    ImagePanel ipMenu;
+    JPanel buttonPanel;
+    JButton jbStart;
+    JButton jbAbout;
+    JButton jbHelp;
+    JButton jbExit;
+    int n = 4;
+
+    Menu() {
+        // ---------- Окно меню
+        MenuFrame = new JFrame("Пятнашки");
+        MenuFrame.setIconImage(Toolkit.getDefaultToolkit().getImage("icon.png"));
+        MenuFrame.setBounds(550, 150, 300, 450);
+        MenuFrame.setResizable(false);
+        MenuFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        ipMenu = new ImagePanel();
+        try {
+            ipMenu.setImage(ImageIO.read(new File("bg.jpg")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        buttonPanel = new JPanel(new GridLayout(4, 1));
+        buttonPanel.add(jbStart = new JButton(new ImageIcon("start.jpg")));
+        buttonPanel.add(jbHelp = new JButton(new ImageIcon("help.jpg")));
+        buttonPanel.add(jbAbout = new JButton(new ImageIcon("about.jpg")));
+        buttonPanel.add(jbExit = new JButton(new ImageIcon("exit.jpg")));
+        ipMenu.add(buttonPanel).setBounds(65, 102, 169, 196);
+        ipMenu.setLayout(null);
+        MenuFrame.add(ipMenu);
+        MenuFrame.setVisible(true);
+
+        // ---------- Вешаем обработчики ----------
+        // ---------- Старт
+        jbStart.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                new LevelChange();
+            }
+        });
+        // ---------- Помощь
+        jbHelp.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Help();
+            }
+        });
+        // ---------- О программе
+        jbAbout.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                About();
+            }
+        });
+        // ---------- Выход
+        jbExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    } // ---------- Конец конструктора
+
+    // ---------- Старт maim
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Menu();
+            }
+        });
+    } // ---------- Конец main
+
+    // --------- О программе ---------- //
+    public void About() {
+        String txt = "<html><center><H2>О программе</H2><br>" +
+                "v.1.00</center></html>";
+        JFrame AboutFrame = new JFrame("О программе");
+        JLabel jlImage = new JLabel(new ImageIcon("15.jpg"));
+        JLabel jlAbout = new JLabel(txt);
+        jlAbout.setHorizontalAlignment(SwingConstants.CENTER);
+        jlAbout.setVerticalAlignment(SwingConstants.TOP);
+        AboutFrame.setIconImage(Toolkit.getDefaultToolkit().getImage("icon.png"));
+        AboutFrame.setBounds(550, 150, 300, 450);
+        AboutFrame.setLayout(new GridLayout(2, 1));
+        AboutFrame.add(jlImage);
+        AboutFrame.add(jlAbout);
+        AboutFrame.setVisible(true);
+    }
+
+    // --------- Помошь ---------- //
+    public void Help() {
+        String txt = "<html><center><H2>Помощь</H2><br>" +
+                "Цель игры — выстроить или переместить костяшки слева на право по возрастанию в коробке и тем самым " +
+                "добиться упорядочивания их по номерам. При этом ставится дополнительная задача - сделать как можно " +
+                "меньше ходов.<br>" +
+                "Для перемещения костяшки нажмите на ту, которую необходимо переместить, и она автоматически " +
+                "переместится.</center></html>";
+        JFrame HelpFrame = new JFrame("Справка");
+        JLabel jlHelp = new JLabel(txt);
+        jlHelp.setHorizontalAlignment(SwingConstants.CENTER);
+        jlHelp.setVerticalAlignment(SwingConstants.TOP);
+        HelpFrame.setIconImage(Toolkit.getDefaultToolkit().getImage("icon.png"));
+        HelpFrame.setBounds(550, 150, 300, 450);
+        HelpFrame.add(jlHelp);
+        HelpFrame.setVisible(true);
+    }
+
+    public class LevelChange implements ActionListener {
+        JDialog jdLevel;
+        JLabel jlLevel;
+        ImagePanel ipLevel;
+        JButton jbStartGame;
+        JButton jbCancel;
+        JRadioButton jrb3x3;
+        JRadioButton jrb4x4;
+        JRadioButton jrb5x5;
+
+        LevelChange() {
+            jdLevel = new JDialog();
+            jdLevel.setModal(true);
+            jdLevel.setResizable(false);
+            jdLevel.setIconImage(Toolkit.getDefaultToolkit().getImage("icon.png"));
+            ipLevel = new ImagePanel();
+            try {
+                ipLevel.setImage(ImageIO.read(new File("bg_field.jpg")));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            ipLevel.setLayout(null);
+            ipLevel.add(jlLevel = new JLabel("Выберите сложность игры"));
+            ipLevel.add(jbStartGame = new JButton(new ImageIcon("start_l.jpg")));
+            ipLevel.add(jbCancel = new JButton(new ImageIcon("exit_l.jpg")));
+            ipLevel.add(jrb3x3 = new JRadioButton("Поле 3x3"));
+            ipLevel.add(jrb4x4 = new JRadioButton("Поле 4x4", true));
+            ipLevel.add(jrb5x5 = new JRadioButton("Поле  5x5"));
+            jlLevel.setHorizontalAlignment(SwingConstants.CENTER);
+            jrb3x3.setOpaque(false);
+            jrb3x3.setFocusPainted(false);
+            jrb4x4.setOpaque(false);
+            jrb4x4.setFocusPainted(false);
+            jrb5x5.setOpaque(false);
+            jrb5x5.setFocusPainted(false);
+            ButtonGroup btLevel = new ButtonGroup();
+            btLevel.add(jrb3x3);
+            btLevel.add(jrb4x4);
+            btLevel.add(jrb5x5);
+            jdLevel.setBounds(600, 250, 200, 220);
+            jlLevel.setBounds(10, 10, 170, 20);
+            jrb3x3.setBounds(50, 40, 100, 10);
+            jrb4x4.setBounds(50, 60, 100, 10);
+            jrb5x5.setBounds(50, 80, 100, 10);
+            jbStartGame.setBounds(45, 110, 102, 29);
+            jbCancel.setBounds(45, 140, 102, 29);
+            jdLevel.add(ipLevel);
+            jrb3x3.addActionListener(this);
+            jrb4x4.addActionListener(this);
+            jrb5x5.addActionListener(this);
+
+            jbStartGame.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    jdLevel.dispose();
+                    MenuFrame.dispose();
+                    new Game(n).createField();
+                }
+            });
+            jbCancel.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    jdLevel.dispose();
+                }
+            });
+
+            jdLevel.setVisible(true);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (jrb3x3.isSelected()) n = 3;
+            else if (jrb4x4.isSelected()) n = 4;
+            else n = 5;
+        }
+    }
+}
